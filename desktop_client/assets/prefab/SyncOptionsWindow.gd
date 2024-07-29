5fca4ae0fdf3fed6348d0d6a64701dd6f22ebac0
Fun Noober <yonts.myles@gmail.com>
2024-07-29 17:03:23 -0400

    Added a security token to the sync server to prevent unauthorized access of pushing of data. Added a configuration file to the sync server to change IP, port, and security token. Added some error handling to the sync server.

--- desktop_client/assets/prefab/SyncOptionsWindow.gd
+++ desktop_client/assets/prefab/SyncOptionsWindow.gd
@@ -19,9 +19,11 @@ func load_options():
 		return
 	options = json.data
 	$MarginContainer/Lines/RemoteEdit.text = options.remote
+	$MarginContainer/Lines/SecurityTokenEdit.text = options.security_token
 
 func _on_save_as_remote_pressed():
 	options.remote = $MarginContainer/Lines/RemoteEdit.text
+	options.security_token = $MarginContainer/Lines/SecurityTokenEdit.text
 	var json_string = JSON.stringify(options)
 	var file_access = FileAccess.open("user://sync_options.json", FileAccess.WRITE)
 	if not file_access:
@@ -34,14 +36,19 @@ func _on_close_requested():
 	hide()
 
 func _on_pull_button_pressed():
-	$PullRequest.request("http://" + options.remote + "/pull")
+	var headers = ["Security-Token: " + $MarginContainer/Lines/SecurityTokenEdit.text]
+	$PullRequest.request("http://" + options.remote + "/pull", headers)
 
 func _on_push_button_pressed():
 	var json = JSON.stringify(SceneManager.documents)
-	var headers = ["Content-Type: application/json"]
+	var headers = ["Content-Type: application/json", "Security-Token:" + $MarginContainer/Lines/SecurityTokenEdit.text]
 	$PushRequest.request("http://" + options.remote + "/push", headers, HTTPClient.METHOD_POST, json)
 
 func _on_pull_request_request_completed(result, response_code, headers, body):
+	if str(response_code) == "401":
+		$MarginContainer/PushSuccessfulPopup/Label.text = "Invalid Security Token"
+		$MarginContainer/PushSuccessfulPopup/AnimationPlayer.play("ShowPushNotification")
+		return
 	var json_string = body.get_string_from_utf8()
 	var json = JSON.new()
 	var err = json.parse(json_string)
@@ -52,6 +59,10 @@ func _on_pull_request_request_completed(result, response_code, headers, body):
 	get_tree().change_scene_to_file("res://main_ui.tscn")
 
 func _on_push_request_request_completed(result, response_code, headers, body):
-	print(body.get_string_from_utf8())
+	if str(response_code) == "401":
+		$MarginContainer/PushSuccessfulPopup/Label.text = "Invalid Security Token"
+		$MarginContainer/PushSuccessfulPopup/AnimationPlayer.play("ShowPushNotification")
+		return
 	if body.get_string_from_utf8() == "Database received":
+		$MarginContainer/PushSuccessfulPopup/Label.text = "Push Successful"
 		$MarginContainer/PushSuccessfulPopup/AnimationPlayer.play("ShowPushNotification")
--- sync_server/server.go
+++ sync_server/server.go
@@ -15,27 +15,52 @@ type Documents struct {
 	TimeOfModification string `json:"time_of_modification"`
 }
 
+type Config struct {
+	SecurityToken string `json:"security_token"`
+	Ip            string `json:"ip"`
+	Port          string `json:"port"`
+}
+
 func main() {
+	config, configErr := loadConfig()
+	if configErr != nil {
+		fmt.Println("Error while loading configuration file")
+		return
+	}
+
 	r := fiber.New()
 	r.Get("/pull", func(c *fiber.Ctx) error {
+		if c.Get("Security-Token") != config.SecurityToken {
+			return c.SendStatus(401)
+		}
+
 		content, fileErr := os.ReadFile("noobdocs.json")
 		var objmap map[string]Documents
 		if fileErr != nil {
 			fmt.Println(fileErr)
+			return c.SendStatus(500)
 		}
+
 		marshalErr := json.Unmarshal(content, &objmap)
 		if marshalErr != nil {
 			fmt.Println(marshalErr)
+			return c.SendStatus(500)
 		}
+
 		return c.JSON(fiber.Map{
 			"documents": objmap,
 		})
 	})
 	r.Post("/push", func(c *fiber.Ctx) error {
+		if c.Get("Security-Token") != config.SecurityToken {
+			return c.SendStatus(401)
+		}
+
 		var bodyString = string(c.Body())
 		file, err := os.Create("noobdocs.json")
 		if err != nil {
 			fmt.Println(err)
+			return c.SendStatus(500)
 		}
 		defer file.Close()
 		file.WriteString(bodyString)
@@ -43,6 +68,21 @@ func main() {
 		return c.SendString("Database received")
 	})
 
-	err := r.Listen("192.168.1.78:8567")
+	err := r.Listen(config.Ip + ":" + config.Port)
 	fmt.Println(err)
+}
+
+func loadConfig() (*Config, error) {
+	var config = new(Config)
+	content, fileErr := os.ReadFile("config.json")
+	if fileErr != nil {
+		fmt.Println(fileErr)
+		return nil, fileErr
+	}
+	jsonMarshallErr := json.Unmarshal(content, &config)
+	if jsonMarshallErr != nil {
+		fmt.Println(jsonMarshallErr)
+		return nil, jsonMarshallErr
+	}
+	return config, nil
 }
